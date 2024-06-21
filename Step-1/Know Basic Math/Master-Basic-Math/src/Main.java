ea73c1113ce00e326bfaa43f6e09bb8cc9d60f8c
Sahil <sahilladhania5@gmail.com>
2024-06-21 13:14:00 +0530

    Added few things.

---
+++ Step-1/Know Basic Math/Master-Basic-Math/src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Count Digits :");
+        countDigit(12345);
+        System.out.println("Reverse Number :");
+        reverseNumber(12345);
+        System.out.println("Check Palindrome :");
+        checkPalindrome(404);
+        System.out.println("Find GCD :");
+        findGCD(50 , 10);
+        System.out.println("Check Armstrong :");
+        checkArmstrong(12345);
+        System.out.println("Return Divisors :");
+        returnDivisors(500);
+        System.out.println("Check Prime :");
+        checkPrime(17);
+    }
+
+//    Basic Math Questions :-
+
+//    Problem : 1
+//    Given a number N. Count the number of digits in N which evenly divide N.
+    public static void countDigit(int n) {
+
+    }
+
+//    Problem : 2
+//    Given an integer N return the reverse of the given number.
+    public static void reverseNumber(int n) {
+
+    }
+
+//    Problem : 3
+//    Given an integer N, return true if it is a palindrome else return false.
+    public static void checkPalindrome(int n) {
+
+    }
+
+//    Problem : 4
+//    Given two integers N1 and N2, find their greatest common divisor.
+    public static void findGCD(int n1 , int n2) {
+
+    }
+
+//    Problem : 5
+//    Given an integer N, return true it is an Armstrong number otherwise return false.
+    public static void checkArmstrong(int n) {
+
+    }
+
+//    Problem : 6
+//    Given an integer N, return all divisors of N.
+    public static void returnDivisors(int n) {
+
+    }
+
+//    Problem : 7
+//    Given an integer N, check whether it is prime or not.
+    public static void checkPrime(int n) {
+
+    }
+
+}
