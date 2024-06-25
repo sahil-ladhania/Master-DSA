0bebb30d05979ca4dc791c7988a6c168004e7ca8
Sahil <sahilladhania5@gmail.com>
2024-06-25 18:11:02 +0530

    Understood Recursion.

--- Step-1/Learn Basic Recursion/Master-Basic-Recursion/src/Main.java
+++ Step-1/Learn Basic Recursion/Master-Basic-Recursion/src/Main.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter n to Find Factorial :");
+        int n = scanner.nextInt();
+        int answer = factorial(n);
+        System.out.println("Factorial of n : " + answer);
+        System.out.println("Enter x to Find Power :");
+        int x = scanner.nextInt();
+        int answer2 = power(x);
+        System.out.println("Power of x : " + answer2);
     }
+
+//     Example 1: Factorial Calculation with Base Case
+    public static int factorial(int n) {
+        //        Base Case
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            //            Recursive Relation
+            int smallerProblem = factorial(n - 1);
+            int biggerProblem = n * smallerProblem;
+            return biggerProblem;
+        }
+    }
+//      Example 1: Factorial Calculation without Base Case ----> Will Give Segmentation Error because of Stack Overflow
+//    public static int factorial(int n) {
+//        System.out.println(n);
+//        return n * factorial(n - 1);
+//    }
+
+//    Example 2: Calculate Power of 2 with Base Case
+    public static int power(int x) {
+        //        Base Case
+        if(x == 0){
+            return 1;
+        }
+        else{
+            //            Recursive Relation
+            int smallerProblem = power(x - 1);
+            int biggerProblem = 2 * smallerProblem;
+            return biggerProblem;
+        }
+    }
+    //    Example 2: Calculate Power of 2 without Base Case ----> Will Give Segmentation Error because of Stack Overflow
+//    public static int power(int x) {
+//        System.out.println(x);
+//        int smallerProblem = power(x - 1);
+//        int biggerProblem = 2 * smallerProblem;
+//        return biggerProblem;
+//    }
+
 }
