64633af7a445d25ef7a3af0a48f8697d231dac66
Sahil <sahilladhania5@gmail.com>
2024-06-30 13:34:00 +0530

    Added few things.

--- Step-1/Learn Basic Recursion/Master-Basic-Recursion/src/FibonacciNumber.java
+++ Step-1/Learn Basic Recursion/Master-Basic-Recursion/src/FibonacciNumber.java
@@ -1,13 +1,18 @@
 public class FibonacciNumber {
     public static void main(String[] args) {
-        int answer = fibonacci(6);
+        int answer = fibonacci(5);
         System.out.println(answer);
     }
 
     public static int fibonacci(int n) {
-        if(n <= 1){
-            return n;
+        //        Base Condition 1
+        if(n == 0){
+            return 0;
         }
-        return (fibonacci(n-1) + fibonacci(n-2));
+        //        Base Condition 2
+        else if(n == 1){
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
