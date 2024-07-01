fe9b5649debbe677380563efe190f9a61219e257
Sahil <sahilladhania5@gmail.com>
2024-07-01 21:08:13 +0530

    Added few things.

--- Step-2/Searching/Linear Search/Master-Linear-Search/src/Main.java
+++ Step-2/Searching/Linear Search/Master-Linear-Search/src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Mastering Linear Search :");
+        int[] number = {12 , 23 , 22 , 30 , 19 , 2 , 9};
+        int target = 19;
+        int answer = linearSearch(number , target);
+        System.out.println(answer);
+        int answer2 = linearSearch2(number , target);
+        System.out.println(answer2);
     }
-}
+    
+    //   Returning Index
+    public static int linearSearch(int[] arr , int target) {
+        if(arr.length == 0){
+            return -1;
+        }
+        else{
+            for (int index = 0; index < arr.length; index++) {
+                int element = arr[index];
+                if(element == target){
+                    return index;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //   Returning Element
+    public static int linearSearch2(int[] arr , int target) {
+        if(arr.length == 0){
+            return -1;
+        }
+        else{
+            for (int index = 0; index < arr.length; index++) {
+                int element = arr[index];
+                if(element == target){
+                    return element;
+                }
+            }
+        }
+        return -1;
+    }
+ }
