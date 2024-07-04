ec387c426e475749a26b5898b428b62e34262d54
Sahil <sahilladhania5@gmail.com>
2024-07-04 16:55:09 +0530

    Added few things.

--- Step-2/Searching/Linear Search/Master-Linear-Search/src/LinearSearchQuestions.java
+++ Step-2/Searching/Linear Search/Master-Linear-Search/src/LinearSearchQuestions.java
@@ -13,52 +13,52 @@ public class LinearSearchQuestions {
 //        Question10();
     }
 
-//        Given an array [3, 5, 1, 2, 4], find the index of the number 2.
+//        Given an array [3, 5, 1, 2, 4], find the index of the number 2. -----> Output : 3
     public static void Question1(int arr1) {
 
     }
 
-//        Search for the number 7 in the array [10, 20, 30, 40, 50]. What is the output?
+//        Search for the number 7 in the array [10, 20, 30, 40, 50]. What is the output? -----> Output : -1 or Not Found
     public static void Question2(int arr2) {
 
     }
 
-//        Given an array of strings ["apple", "banana", "cherry", "date"], find the index of the string "cherry".
+//        Given an array of strings ["apple", "banana", "cherry", "date"], find the index of the string "cherry". -----> Output : 2
     public static void Question3(int arr3) {
 
     }
 
-//        Find the first occurrence of the number 5 in the array [1, 2, 3, 4, 5, 5, 5].
+//        Find the first occurrence of the number 5 in the array [1, 2, 3, 4, 5, 5, 5]. -----> Output : 4
     public static void Question4(int arr4) {
 
     }
 
-//        Check if the number 15 is present in the array [5, 10, 15, 20, 25].
+//        Check if the number 15 is present in the array [5, 10, 15, 20, 25]. -----> Output : True
     public static void Question5(int arr5) {
 
     }
 
-//        Search for the number 100 in the array [1, 2, 3, 4, 5]. What is the output?
+//        Search for the number 100 in the array [1, 2, 3, 4, 5]. What is the output? -----> Output : -1 or Not Found
     public static void Question6(int arr6) {
 
     }
 
-//        Given an array [7, 14, 21, 28, 35], find the index of the number 21.
+//        Given an array [7, 14, 21, 28, 35], find the index of the number 21. -----> Output : 2
     public static void Question7(int arr7) {
 
     }
 
-//        Search for the string "orange" in the array ["red", "blue", "green", "yellow"]. What is the output?
+//        Search for the string "orange" in the array ["red", "blue", "green", "yellow"]. What is the output? -----> Output : -1 or Not Found
     public static void Question8(int arr8) {
 
     }
 
-//        Given an array [8, 6, 7, 5, 3, 0, 9], find the index of the number 0.
+//        Given an array [8, 6, 7, 5, 3, 0, 9], find the index of the number 0. -----> Output : 5
     public static void Question9(int arr9) {
 
     }
 
-//        Check if the number 12 is present in the array [11, 22, 33, 44, 55].
+//        Check if the number 12 is present in the array [11, 22, 33, 44, 55]. -----> Output : False
     public static void Question10(int arr10) {
 
     }
