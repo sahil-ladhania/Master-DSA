36a2834969070a4a6282d33678753133b5176f00
Sahil <sahilladhania5@gmail.com>
2024-07-12 15:18:41 +0530

    Added few things.

---
+++ Step-2/Sorting/Merge Sort/Master-Merge-Sort/src/MergeSort1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MergeSort1 {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        System.out.println("Unsorted Array : " + Arrays.toString(arr));
+        mergeSort1(arr);
+        System.out.println("Sorted Array : " + Arrays.toString(arr));
+    }
+
+    public static void mergeSort1(int[] arr) {
+
+    }
+}
