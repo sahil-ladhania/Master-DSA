fda166431a908d179449847f0bf8f00481cec081
Sahil <sahilladhania5@gmail.com>
2024-07-16 13:44:08 +0530

    Added few things.

--- Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/LargestElement_1.java
+++ Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/LargestElement_1.java
@@ -1,5 +1,58 @@
+import java.util.Arrays;
+
 public class LargestElement_1 {
     public static void main(String[] args) {
-
+        int[] arr = {1,3,5,2,4,5,4,9};
+        largestElementBruteForce(arr);
+        int n = arr.length;
+        System.out.println("Brute Force Answer : " + arr[n-1]);
+        String betterAnswer = largestElementBetter(arr);
+        System.out.println("Better Answer : " + betterAnswer);
+        int optimalAnswer = largestElementOptimal(arr);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
+
+    // Brute Force Approach -----> TC : O(n^2)
+    public static int minIndex(int[] arr , int currentIndex) {
+        int minIndex = currentIndex;
+        for (int i = currentIndex + 1; i < arr.length; i++) {
+            if(arr[minIndex] > arr[i]){
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+    public static void swapElements(int[] arr , int minIndex , int currentIndex) {
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[currentIndex];
+        arr[currentIndex] = temp;
+    }
+    public static void largestElementBruteForce(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = minIndex(arr , i);
+            if(minIndex != i){
+                swapElements(arr , minIndex , i);
+            }
+        }
+    }
+
+    // Better Approach
+    public static String largestElementBetter(int[] arr) {
+        String str = "We dont have a better Solution in this Question !!!";
+        return str;
+    }
+
+    // Optimal Approach ---> TC : O(n)
+    public static int largestElementOptimal(int[] arr) {
+        int n = arr.length;
+        int largestElement = arr[0];
+        for (int i = 0; i < n; i++) {
+            if(largestElement < arr[i]){
+                largestElement = arr[i];
+            }
+        }
+        return largestElement;
+    }
+
 }
--- Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/SecondLargestElement_2.java
+++ Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/SecondLargestElement_2.java
@@ -1,5 +1,70 @@
+import java.util.Arrays;
+
 public class SecondLargestElement_2 {
     public static void main(String[] args) {
+        int[] arr = {1,4,2,7,5,7};
+        int bruteForceAnswer = secondLargestElementBruteForce(arr);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        int betterAnswer = secondLargestElementBetter(arr);
+        System.out.println("Better Answer : " + betterAnswer);
+        int optimalAnswer = secondLargestElementOptimal(arr);
+        System.out.println("Optimal Answer : " + optimalAnswer);
+    }
 
+    // Brute Force Approach ----> TC : O(n log n)
+    public static int secondLargestElementBruteForce(int[] arr) {
+        int n = arr.length;
+        if(n < 2){
+            return -1;
+        }
+        Arrays.sort(arr);
+        int largest = arr[n-1];
+        for (int i = n-2; i >= 0 ; i--) {
+            if(arr[i] != largest){
+               return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    // Better Approach ---> TC : O(2n)
+    public static int secondLargestElementBetter(int[] arr) {
+        int n = arr.length;
+        if(n < 2){
+            return -1;
+        }
+        int largest = arr[0];
+        int secondLargest = -1;
+        for (int i = 0; i < n; i++) {
+            if(largest < arr[i]){
+                largest = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if((arr[i] > secondLargest) && (arr[i] < largest)){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    // Optimal Approach ---> TC : O(n)
+    public static int secondLargestElementOptimal(int[] arr) {
+        int n = arr.length;
+        if(n < 2){
+            return -1;
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if(largest < arr[i]){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if((arr[i] > secondLargest) && (arr[i] != largest)){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
     }
 }
