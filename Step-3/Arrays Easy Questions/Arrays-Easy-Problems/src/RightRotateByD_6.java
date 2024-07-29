1da0256d74871989de663dc04a9591e2ac5ea4ae
Sahil <sahilladhania5@gmail.com>
2024-07-30 01:39:12 +0530

    Done few changes.

--- Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/RightRotateByD_6.java
+++ Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/RightRotateByD_6.java
@@ -2,35 +2,71 @@ import java.util.Arrays;
 
 public class RightRotateByD_6 {
     public static void main(String[] args) {
-        System.out.println("The Brute Force Approach Striver taught is Complex and that basically took less TC than Optimal but SC is better in Optimal.");
         int[] arr1 = {1,2,3,4,5};
         int[] arr2 = {1,2,3,4,5};
         int[] arr3 = {1,2,3,4,5};
         bruteForceRightRotateByD(arr1 , 2);
         System.out.println("Brute Force Answer : " + Arrays.toString(arr1));
-        betterRightRotateByD(arr2 , 2);
-        System.out.println("Better Answer : " + Arrays.toString(arr2));
+        String betterAnswer = betterRightRotateByD(arr2 , 2);
+        System.out.println("Better Answer : " + betterAnswer);
         optimalRightRotateByD(arr3 , 2);
         System.out.println("Optimal Answer : " + Arrays.toString(arr3));
     }
 
-    public static void reverse(int[] arr, int start, int end) {
-
-    }
-
-    // Brute Force Approach -----> TC :
+    // Brute Force Approach
     public static void bruteForceRightRotateByD(int[] arr, int K) {
+        int N = arr.length;
+        if(N == 0){
+            return;
+        }
+        K = K % N;
+        if(K > N){
+            return;
+        }
+        //  Understand the Working of Loops
+        int[] temp = new int[K];
+        for (int i = N - K; i < N; i++) {
+            temp[i - N + K] = arr[i];
+        }
+        for (int i = N - K - 1; i >= 0; i--) {
+            arr[i + K] = arr[i];
+        }
+        for (int i = 0; i < K; i++) {
+            arr[i] = temp[i];
+        }
+    }
+    /*
+    TC : O(3n) ---> There are three main steps, each of which runs in  O(n)  or  O(K) . The combined operations still
+                    result in  O(n)  since  O(n) + O(n) + O(K) = O(3n) = O(n) .
+    SC : O(k) ---> We use a temporary array of size  K  to store the last K elements.
+    */
 
+    // Better Approach
+    public static String betterRightRotateByD(int[] arr, int K) {
+        String str = "There is no better solution for this problem.";
+        return str;
     }
 
-    // Better Approach -----> TC :
-    public static void betterRightRotateByD(int[] arr, int K) {
-
+    // Optimal Approach
+    public static void reverse(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
-
-    // Optimal Approach ---> TC :
     public static void optimalRightRotateByD(int[] arr, int K) {
-
+        int n = arr.length;
+        K = K % n;
+        reverse(arr , 0 , n-1);
+        reverse(arr , 0 , K - 1);
+        reverse(arr , K , n - 1);
     }
+    /*
+    TC : O(n) ---> Each reverse operation takes  O(n)  time in total since the array is reversed in parts.
+    SC : O(1) ---> No extra space is used other than a few variables for swapping elements.
+    */
 
 }
--- Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/RightRotateByOne_5.java
+++ Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/RightRotateByOne_5.java
@@ -2,28 +2,52 @@ import java.util.Arrays;
 
 public class RightRotateByOne_5 {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        bruteForceRightRotateBy1(arr);
-        System.out.println("Brute Force Answer : " + Arrays.toString(arr));
-        betterRightRotateBy1(arr);
-        System.out.println("Better Answer : " + Arrays.toString(arr));
-        optimalRightRotateBy1(arr);
-        System.out.println("Optimal Answer : " + Arrays.toString(arr));
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {1,2,3,4,5};
+        bruteForceRightRotateBy1(arr1);
+        System.out.println("Brute Force Answer : " + Arrays.toString(arr1));
+        String betterAnswer = betterRightRotateBy1(arr1);
+        System.out.println("Better Answer : " + betterAnswer);
+        optimalRightRotateBy1(arr2);
+        System.out.println("Optimal Answer : " + Arrays.toString(arr2));
     }
 
-    // Brute Force Approach -----> TC :
+    // Brute Force Approach
     public static void bruteForceRightRotateBy1(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+        int lastElement = arr[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            temp[i + 1] = arr[i];
+        }
+        temp[0] = lastElement;
+        for (int j = 0; j < n; j++) {
+            arr[j] = temp[j];
+        }
+    }
+    /*
+    TC : O(2n) ---> The loop iterates  n  times to copy elements to the temporary array and back to the original array.
+    SC : O(n) ---> The temporary array temp of size  n  is used.
+    */
 
+    // Better Approach
+    public static String betterRightRotateBy1(int[] arr) {
+        String str = "There is no better solution for this problem.";
+        return str;
     }
 
-    // Better Approach -----> TC :
-    public static void betterRightRotateBy1(int[] arr) {
-
-    }
-
-    // Optimal Approach ---> TC :
+    // Optimal Approach
     public static void optimalRightRotateBy1(int[] arr) {
-
+        int n = arr.length;
+        int lastElement = arr[n - 1];
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = lastElement;
     }
+    /*
+    TC : O(n) ---> The loop iterates  n - 1  times, performing a constant-time assignment in each iteration.
+    SC : O(1) ---> Only a single additional variable (lastElement) is used, regardless of the input size.
+    */
 
 }
