380f677b2a16a7353a81000059ba300e84eb4185
Sahil <sahilladhania5@gmail.com>
2024-07-29 18:55:31 +0530

    Done few changes.

--- Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/CheckIfArraySorted_3.java
+++ Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/CheckIfArraySorted_3.java
@@ -1,27 +1,27 @@
-public class CheckIfArraySortedRotated_3 {
+public class CheckIfArraySorted_3 {
     public static void main(String[] args) {
-        int[] arr = {3,4,5,1,2};
+        int[] arr = {1,2,3,4};
         System.out.println("There's no better method to solve this Question with better TC !!!");
-        boolean bruteForceAnswer = bruteForceCheckSortedRotated(arr);
+        boolean bruteForceAnswer = bruteForceCheckSorted(arr);
         System.out.println("Brute Force Answer : " + bruteForceAnswer);
-        boolean betterAnswer = betterCheckSortedRotated(arr);
+        boolean betterAnswer = betterCheckSorted(arr);
         System.out.println("Better Answer : " + betterAnswer);
-        boolean optimalAnswer = optimalCheckSortedRotated(arr);
+        boolean optimalAnswer = optimalCheckSorted(arr);
         System.out.println("Optimal Answer : " + optimalAnswer);
     }
 
     // Brute Force Approach
-    public static boolean bruteForceCheckSortedRotated(int[] arr) {
+    public static boolean bruteForceCheckSorted(int[] arr) {
         return true;
     }
 
     // Better Approach
-    public static boolean betterCheckSortedRotated(int[] arr) {
+    public static boolean betterCheckSorted(int[] arr) {
         return true;
     }
 
     // Optimal Approach
-    public static boolean optimalCheckSortedRotated(int[] arr){
+    public static boolean optimalCheckSorted(int[] arr){
         return true;
     }
 
--- Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/LinearSearch_8.java
+++ Step-3/Arrays Easy Questions/Arrays-Easy-Problems/src/LinearSearch_8.java
@@ -2,7 +2,7 @@ public class LinearSearch_8 {
     public static void main(String[] args) {
         System.out.println("Brute Force is the most Optimal Solution for this Problem.");
         int[] arr = {1,2,3,4,5};
-        int target = 21;
+        int target = 4;
         String bruteForceAnswer = bruteForceLinearSearch(arr , target);
         System.out.println("Brute Force Answer : " + bruteForceAnswer);
         String betterAnswer = betterLinearSearch(arr , target);
@@ -11,22 +11,35 @@ public class LinearSearch_8 {
         System.out.println("Optimal Answer : " + optimalAnswer);
     }
 
-    // Brute Force Approach -----> TC : O(n)
+    // Brute Force Approach
     public static String bruteForceLinearSearch(int[] arr , int target) {
         String str2 = "There is no Brute Force Solution for this Problem.";
         System.out.println(str2);
         return str2;
     }
 
-    // Better Approach ---> TC : O(n)
+    // Better Approach
     public static String betterLinearSearch(int[] arr , int target) {
         String str1 = "There is no better Solution for this Problem.";
         System.out.println(str1);
         return str1;
     }
 
-    // Optimal Approach ---> TC : O(n)
+    // Optimal Approach
     public static boolean optimalForceLinearSearch(int[] arr , int target) {
-        return true;
+        int n = arr.length;
+        for(int i = 0; i < n; i++) {
+            if(target == arr[i]){
+                return true;
+            }
+        }
+        return false;
     }
+    /*
+    TC : O(n) ---> The for loop iterates through the array once, comparing each element to the target. This results
+                   in a linear time complexity of  O(n) .
+    SC : O(1) ---> The space complexity is  O(1)  because only a few extra variables (n and i) are used. No additional
+                   space that grows with the input size is required.
+    */
+
 }
