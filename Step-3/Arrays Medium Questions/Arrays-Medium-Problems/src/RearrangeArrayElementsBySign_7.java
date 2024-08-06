bf931dd9179c0a5ba3c4df850c4185a3f3352778
Sahil <sahilladhania5@gmail.com>
2024-08-06 19:40:28 +0530

    Done few changes.

--- Step-3/Arrays Medium Questions/Arrays-Medium-Problems/src/RearrangeArrayElementsBySign_7.java
+++ Step-3/Arrays Medium Questions/Arrays-Medium-Problems/src/RearrangeArrayElementsBySign_7.java
@@ -1,23 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RearrangeArrayElementsBySign_7 {
     public static void main(String[] args) {
-
+        int[] arr1 = {1,2,-4,-5};
+        int[] arr2 = {1,2,-4,-5};
+        int[] arr3 = {1,2,-4,-5};
+        bruteForceRearrangeArrElementsBySign(arr1);
+        System.out.println("Brute Force Answer : " + Arrays.toString(arr1));
+        String betterAnswer = betterRearrangeArrElementsBySign(arr2);
+        System.out.println("Better Answer : " + betterAnswer);
+        int[] optimalAnswer = optimalRearrangeArrElementsBySign(arr3);
+        System.out.println("Optimal Answer : " + Arrays.toString(optimalAnswer));
     }
 
+    // Type-1
     // Brute Force Approach
+    public static void bruteForceRearrangeArrElementsBySign(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> positiveNum = new ArrayList<>(n/2);
+        ArrayList<Integer> negativeNum = new ArrayList<>(n/2);
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > 0){
+                positiveNum.add(arr[i]);
+            }
+            else{
+                negativeNum.add(arr[i]);
+            }
+        }
+        for (int j = 0; j < n/2; j++) {
+            arr[2 * j] = positiveNum.get(j);
+            arr[2 * j + 1] = negativeNum.get(j);
+        }
+    }
     /*
-    TC : O() --->
-    SC : O() --->
+    TC : O(2n) --->  Iterates over the array twice: once to separate positive and negative numbers, and once to rearrange them.
+    SC : O(n) ---> Uses additional space for two ArrayLists to store positive and negative numbers.
     */
 
     // Better Approach
-    /*
-    TC : O() --->
-    SC : O() --->
-    */
+    public static String betterRearrangeArrElementsBySign(int[] arr) {
+        String str = "There is no better solution for this Problem.";
+        return str;
+    }
 
     // Optimal Approach
+    public static int[] optimalRearrangeArrElementsBySign(int[] arr) {
+        int n = arr.length;
+        int[] ansArr = new int[n];
+        int posIndex = 0;
+        int negIndex = 1;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > 0){
+                ansArr[posIndex] = arr[i];
+                posIndex = posIndex + 2;
+            }
+            else{
+                ansArr[negIndex] = arr[i];
+                negIndex = negIndex + 2;
+            }
+        }
+        return ansArr;
+    }
     /*
-    TC : O() --->
-    SC : O() --->
+    TC : O(n) ---> Iterates over the array once to rearrange elements.
+    SC : O(n) ---> Uses additional array ansArr to store the rearranged elements.
     */
+
+    // Type-2
 }
--- Step-3/Arrays Medium Questions/Arrays-Medium-Problems/src/StockBuyAndSell_6.java
+++ Step-3/Arrays Medium Questions/Arrays-Medium-Problems/src/StockBuyAndSell_6.java
@@ -1,23 +1,55 @@
 public class StockBuyAndSell_6 {
     public static void main(String[] args) {
-
+        int[] arr = {7,1,5,3,6,4};
+        int bruteForceAnswer = bruteForceStockBuyAndSell(arr);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        String betterAnswer = betterStockBuyAndSell(arr);
+        System.out.println("Better Answer : " + betterAnswer);
+        int optimalAnswer = optimalStockBuyAndSell(arr);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
 
     // Brute Force Approach
+    public static int bruteForceStockBuyAndSell(int[] arr) {
+        int n = arr.length;
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            int profit = 0;
+            for (int j = i; j < n; j++) {
+                if(i < j){
+                    profit = arr[j] - arr[i];
+                    if(profit > maxProfit){
+                        maxProfit = profit;
+                    }
+                }
+            }
+        }
+        return maxProfit;
+    }
     /*
-    TC : O() --->
-    SC : O() --->
+    TC : O(n^2) ---> Due to the nested loops iterating through all possible pairs of days.
+    SC : O(1) ---> Only a few extra variables are used.
     */
 
     // Better Approach
-    /*
-    TC : O() --->
-    SC : O() --->
-    */
+    public static String betterStockBuyAndSell(int[] arr) {
+        String str = "There is no better solution for this Problem.";
+        return str;
+    }
 
     // Optimal Approach
+    public static int optimalStockBuyAndSell(int[] arr) {
+        int n = arr.length;
+        int profit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            minPrice = Math.min(minPrice , arr[i]);
+            profit = Math.max(profit , arr[i] - minPrice);
+        }
+        return profit;
+    }
     /*
-    TC : O() --->
-    SC : O() --->
+    TC : O(n) ---> Achieved by a single pass through the array to compute the maximum profit.
+    SC : O(1) ---> Only a few extra variables are used.
     */
 }
