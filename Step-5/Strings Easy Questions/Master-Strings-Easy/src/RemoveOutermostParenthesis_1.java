abd097591ac57a67fef7556b3d9df9f7efa3f755
Sahil <sahilladhania5@gmail.com>
2024-08-20 11:42:54 +0530

    Done few changes.

--- Step-5/Strings Easy Questions/Master-Strings-Easy/src/RemoveOutermostParenthesis_1.java
+++ Step-5/Strings Easy Questions/Master-Strings-Easy/src/RemoveOutermostParenthesis_1.java
@@ -3,30 +3,75 @@ public class RemoveOutermostParenthesis_1 {
         String str1 = "(()())(())";
         String str2 = "(()())(())";
         String str3 = "(()())(())";
-        bruteForceRemoveOutermostParenthesis(str1);
-        betterRemoveOutermostParenthesis(str2);
-        optimalRemoveOutermostParenthesis(str3);
+        String bruteForceAnswer = bruteForceRemoveOutermostParenthesis(str1);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        String betterAnswer = betterRemoveOutermostParenthesis(str2);
+        System.out.println("Better Answer : " + betterAnswer);
+        String optimalAnswer = optimalRemoveOutermostParenthesis(str3);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
 
     // Brute Force Approach
-    public static void bruteForceRemoveOutermostParenthesis(String str) {
-
+    public static String bruteForceRemoveOutermostParenthesis(String str) {
+        int lengthofStr = str.length();
+        String ansStr = "";
+        String subStr = "";
+        int counter = 0;
+        int start = 0;
+        for (int i = 0; i < lengthofStr; i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                counter++;
+                if(counter == 1){
+                    start = i;
+                }
+            }
+            else if (ch == ')') {
+                counter--;
+                if(counter == 0){
+                    subStr = str.substring(start + 1 , i);
+                    ansStr = ansStr + subStr;
+                }
+            }
+        }
+        return ansStr;
     }
     /*
+    TC : O(n) ---> The algorithm processes each character in the string exactly once.
+    SC : O(n) ---> The space required is proportional to the size of the input string due to storing substrings and the final result.
     */
 
     // Better Approach
-    public static void betterRemoveOutermostParenthesis(String str) {
-
+    public static String betterRemoveOutermostParenthesis(String str) {
+        String strr = "There is no better solution for this Problem.";
+        return strr;
     }
-    /*
-     */
 
     // Optimal Approach
-    public static void optimalRemoveOutermostParenthesis(String str) {
-
+    public static String optimalRemoveOutermostParenthesis(String str) {
+        int lengthofStr = str.length();
+        StringBuilder ansStr = new StringBuilder();
+        int counter = 0;
+        int start = 0;
+        for (int i = 0; i < lengthofStr; i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                counter++;
+                if (counter == 1) {
+                    start = i;
+                }
+            } else if (ch == ')') {
+                counter--;
+                if (counter == 0) {
+                    ansStr.append(str, start + 1, i);
+                }
+            }
+        }
+        return ansStr.toString();
     }
     /*
-     */
+    TC : O(n) ---> The algorithm iterates through each character of the string once.
+    SC : O(1) ---> Space used is constant beyond the output string, as we avoid additional storage for intermediate substrings.
+    */
 
 }
--- Step-5/Strings Easy Questions/Master-Strings-Easy/src/RotateString_6.java
+++ Step-5/Strings Easy Questions/Master-Strings-Easy/src/RotateString_6.java
@@ -1,18 +1,59 @@
+import java.util.spi.ToolProvider;
+
 public class RotateString_6 {
     public static void main(String[] args) {
-
+        String s = "abcde";
+        String goal = "cdeab";
+        boolean bruteForceAnswer = bruteForceRotateString(s , goal);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        String betterAnswer = betterRotateString(s , goal);
+        System.out.println("Better Answer : " + betterAnswer);
+        boolean optimalAnswer = optimalRotateString(s , goal);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
 
     // Brute Force Approach
+    public static boolean bruteForceRotateString(String s , String goal) {
+        int n = s.length();
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        for (int i = 0; i < n; i++) {
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (rotated.equals(goal)) {
+                return true;
+            }
+        }
+        return false;
+    }
     /*
-     */
+    TC : O(n^2) ---> Generating and comparing each rotation takes O(n) time, and there are n rotations.
+    SC : O(n) ---> Each rotation requires O(n) space for the new string.
+    */
 
     // Better Approach
-    /*
-     */
+    public static String betterRotateString(String s , String goal) {
+        String str = "There is no better solution for this Problem.";
+        return str;
+    }
 
     // Optimal Approach
+    public static boolean optimalRotateString(String s , String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        if(s.equals(goal)){
+            return true;
+        }
+        String concatenated_s = s + s;
+        if(concatenated_s.contains(goal)){
+            return true;
+        }
+        return false;
+    }
     /*
-     */
+    TC : O(n) ---> Because the most expensive operation is the substring search.
+    SC : O(n) ---> Due to the concatenation of the string.
+    */
 
 }
