eefcfb51d5197bc4cfb9b4c053d1c2c8226277ed
Sahil <sahilladhania5@gmail.com>
2024-08-22 09:33:26 +0530

    Done few changes.

--- Step-5/Strings Medium Questions/Master-Strings-Medium/src/ImplementAtoi_4.java
+++ Step-5/Strings Medium Questions/Master-Strings-Medium/src/ImplementAtoi_4.java
@@ -1,5 +1,24 @@
 public class ImplementAtoi_4 {
     public static void main(String[] args) {
-
+        String s = "1337c0d3";
+        int bruteForceAnswer = bruteForceImplementAtoi(s);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        int betterAnswer = betterImplementAtoi(s);
+        System.out.println("Better Answer : " + betterAnswer);
+        int optimalAnswer = optimalImplementAtoi(s);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
+
+    public static int bruteForceImplementAtoi(String s) {
+        return -1;
+    }
+
+    public static int betterImplementAtoi(String s) {
+        return -1;
+    }
+
+    public static int optimalImplementAtoi(String s) {
+        return -1;
+    }
+
 }
