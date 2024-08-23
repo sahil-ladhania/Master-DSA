70e80586e7359962c3fb173831932c4ac376dace
Sahil <sahilladhania5@gmail.com>
2024-08-23 16:51:02 +0530

    Done few changes.

--- Step-5/Strings Medium Questions/Master-Strings-Medium/src/CountNumberOfSubStrings_5.java
+++ Step-5/Strings Medium Questions/Master-Strings-Medium/src/CountNumberOfSubStrings_5.java
@@ -1,5 +1,24 @@
 public class CountNumberOfSubStrings_5 {
     public static void main(String[] args) {
-
+        String s = "abaaca";
+        int bruteForceAnswer = bruteForceCountNumberOfSubStrings(s , 1);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        int betterAnswer = betterCountNumberOfSubStrings(s , 1);
+        System.out.println("Better Answer : " + betterAnswer);
+        int optimalAnswer = optimalCountNumberOfSubStrings(s , 1);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
+
+    public static int bruteForceCountNumberOfSubStrings(String s , int K) {
+        return -1;
+    }
+
+    public static int betterCountNumberOfSubStrings(String s , int K) {
+        return -1;
+    }
+
+    public static int optimalCountNumberOfSubStrings(String s , int K) {
+        return -1;
+    }
+
 }
--- Step-5/Strings Medium Questions/Master-Strings-Medium/src/ReverseEveryWordInAString_7.java
+++ Step-5/Strings Medium Questions/Master-Strings-Medium/src/ReverseEveryWordInAString_7.java
@@ -1,5 +1,38 @@
 public class ReverseEveryWordInAString_7 {
     public static void main(String[] args) {
-
+        String s = "  hello world  ";
+        String bruteForceAnswer = bruteForceReverseEveryWordInAString(s);
+        System.out.println("Brute Force Answer : " + bruteForceAnswer);
+        String betterAnswer = betterReverseEveryWordInAString(s);
+        System.out.println("Better Answer : " + betterAnswer);
+        String optimalAnswer = optimalReverseEveryWordInAString(s);
+        System.out.println("Optimal Answer : " + optimalAnswer);
     }
+
+    public static String bruteForceReverseEveryWordInAString(String s) {
+        String str = "There is no brute force solution for this Problem.";
+        return str;
+    }
+
+    public static String betterReverseEveryWordInAString(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder ans = new StringBuilder();
+        for(int i = words.length - 1; i >= 0; i--){
+            ans.append(words[i]);
+            if(i != 0){
+                ans.append(" ");
+            }
+        }
+        return ans.toString();
+    }
+    /*
+    TC : O(n) ---> Each operation (trimming, splitting, and reversing) iterates through the string once, where n is the length of the input string. Thus, the overall time complexity is linear with respect to the length of the input.
+    SC : O(n) ---> The space complexity is linear because the algorithm uses extra space for storing the split words array and the resulting StringBuilder, both of which can be proportional to the length of the input string.
+    */
+
+    public static String optimalReverseEveryWordInAString(String s) {
+        String str = "There can be solution where we can reduce SC to O(1).";
+        return str;
+    }
+
 }
