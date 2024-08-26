5906292a76632faab262c95069f672f100c093fd
Sahil <sahilladhania5@gmail.com>
2024-08-26 18:49:31 +0530

    Done few changes.

--- Step-4/BS Easy Questions/Master-BS-Easy-Problems/src/BinarySearch_1.java
+++ Step-4/BS Easy Questions/Master-BS-Easy-Problems/src/BinarySearch_1.java
@@ -10,18 +10,18 @@ public class BinarySearch_1 {
     // Iterative Approach
     public static boolean iterativeBinarySearch(int[] arr , int target) {
         int n = arr.length;
-        int low = 0;
-        int high = n-1;
-        while (low <= high){
-            int mid = (low + high) / 2;
-            if(arr[mid] == target){
+        int start = 0;
+        int end = n-1;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if(target == arr[mid]){
                 return true;
             }
-            else if (arr[mid] < target) {
-                low = mid + 1;
+            else if (target > arr[mid]) {
+                start = mid + 1;
             }
             else{
-                high = mid - 1;
+                end = mid - 1;
             }
         }
         return false;
@@ -37,11 +37,11 @@ public class BinarySearch_1 {
             return false;
         }
         else{
-            int mid = (low + high) / 2;
-            if(arr[mid] == target){
+            int mid = low + (high - low) / 2;
+            if(target == arr[mid]){
                 return true;
             }
-            else if (arr[mid] < target) {
+            else if (target > arr[mid]) {
                 return recursiveBinarySearch(arr , mid + 1 , high , target);
             }
             else{
@@ -54,4 +54,4 @@ public class BinarySearch_1 {
     SC : O(log n) ---> Because of the additional space required by the call stack. In the worst case, the recursion depth can go up to log n, which contributes to the space complexity.
     */
 
-}
+} 
