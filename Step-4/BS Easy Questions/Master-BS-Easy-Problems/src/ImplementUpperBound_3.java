1effbddf132ee5c9c63e3cbd76b5a828a0b7bdd1
Sahil <sahilladhania5@gmail.com>
2024-08-28 15:38:17 +0530

    Done few changes.

--- Step-4/BS Easy Questions/Master-BS-Easy-Problems/src/ImplementUpperBound_3.java
+++ Step-4/BS Easy Questions/Master-BS-Easy-Problems/src/ImplementUpperBound_3.java
@@ -33,17 +33,17 @@ public class ImplementUpperBound_3 {
     // Optimal Approach
     public static int optimalUpperBound(int[] arr , int x) {
         int n = arr.length;
-        int low = 0;
-        int high = n-1;
+        int start = 0;
+        int end = n-1;
         int ans = n;
-        while (low <= high){
-            int mid = (low + high) / 2;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
             if(arr[mid] > x){
                 ans = mid;
-                high = mid - 1;
+                end = mid - 1;
             }
             else{
-                low = mid + 1;
+                start = mid + 1;
             }
         }
         return ans;
